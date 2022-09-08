7dcc4e36035b882dca0ba8df1b3b30655f7a9f62
FelipeGomezGarcia <feli.goga@gmail.com>
2022-09-08 11:00:26 +0200

    Alta usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UD35-AltaUsuario';
+
+  clientes:any;
+  nombre = document.getElementById("nombre")?.innerHTML;
+  cif = document.getElementById("cif")?.innerHTML;
+  direccion = document.getElementById("direccion")?.innerHTML;
+  grupo = document.getElementById("groups")?.innerHTML;
+
+  addCliente(){
+    this.clientes.add({nombre:this.nombre, cif:this.cif, direccion: this.direccion,grupo: this.grupo});
+    console.log("Hola");
+
+  }
 }
